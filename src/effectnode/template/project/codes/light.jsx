a517a3342c445e8d6d9225e7846bc3e452b0dbbf
wonglok <yellowhappy831@gmail.com>
2024-12-07 00:11:12 +0800

    save

--- src/effectnode/template/project/codes/light.jsx
+++ src/effectnode/template/project/codes/light.jsx
@@ -1,13 +1,50 @@
+import { Environment } from "@react-three/drei";
 import { useEffect } from "react";
 
-export function ToolBox({}) {
-  return <></>;
-}
+export function ToolBox({ useAutoSaveData }) {
+  let lightColor = useAutoSaveData((r) => r.lightColor) || "#ffffff";
+  let intensity = useAutoSaveData((r) => r.intensity) || 1;
 
-export function Runtime({ useStore, io }) {
   return (
     <>
-      <pointLight position={[-1.5, 0.5, 1]}></pointLight>
+      <input
+        type="color"
+        value={lightColor}
+        onChange={(e) => {
+          useAutoSaveData.setState({ lightColor: e.target.value });
+        }}
+      ></input>
+      {/*  */}
+      <input
+        type="range"
+        min={0}
+        max={10}
+        step={0.01}
+        value={intensity}
+        onChange={(e) => {
+          useAutoSaveData.setState({ intensity: e.target.value });
+        }}
+      ></input>
+    </>
+  );
+}
+
+export function Runtime({ useAutoSaveData, io, files }) {
+  let lightColor = useAutoSaveData((r) => r.lightColor) || "#ffffff";
+  let intensity = useAutoSaveData((r) => r.intensity) || 1;
+
+  //
+  return (
+    <>
+      <pointLight
+        intensity={intensity}
+        color={lightColor}
+        position={[-1.5, 0.5, 1]}
+      ></pointLight>
+
+      <Environment
+        files={[files["/yoyo/sunflowers_puresky_1k.hdr"]]}
+      ></Environment>
     </>
   );
 }
