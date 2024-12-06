0bfef64f3d53eed45c2c6376a30b5abe1826d51a
wonglok <yellowhappy831@gmail.com>
2024-12-06 23:35:46 +0800

    save

--- src/effectnode/projects/space-me/codes/light.jsx
+++ src/effectnode/projects/space-me/codes/light.jsx
@@ -1,13 +1,42 @@
 import { useEffect } from "react";
 
-export function ToolBox({}) {
-  return <></>;
-}
+export function ToolBox({ useBoxData }) {
+  let lightColor = useBoxData((r) => r.lightColor) || "#ffffff";
+  let intensity = useBoxData((r) => r.intensity) || 1;
 
-export function Runtime({ useStore, io }) {
   return (
     <>
-      <pointLight position={[-1.5, 0.5, 1]}></pointLight>
+      <input
+        type="color"
+        value={lightColor}
+        onChange={(e) => {
+          useBoxData.setState({ lightColor: e.target.value });
+        }}
+      ></input>
+      <input
+        type="range"
+        min={0}
+        max={10}
+        step={0.01}
+        value={intensity}
+        onChange={(e) => {
+          useBoxData.setState({ intensity: e.target.value });
+        }}
+      ></input>
+    </>
+  );
+}
+
+export function Runtime({ useBoxData, io }) {
+  let lightColor = useBoxData((r) => r.lightColor) || "#ffffff";
+  let intensity = useBoxData((r) => r.intensity) || 1;
+  return (
+    <>
+      <pointLight
+        intensity={intensity}
+        color={lightColor}
+        position={[-1.5, 0.5, 1]}
+      ></pointLight>
     </>
   );
 }
