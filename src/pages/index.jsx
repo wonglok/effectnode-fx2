093f924fc1d015a6e8dca86ea33f2cc7abf00b04
wonglok <yellowhappy831@gmail.com>
2024-12-07 14:22:03 +0800

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,27 @@
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { EffectNode } from "effectnode-developer-tools/effectnode-runtime/EffectNode";
 
 export default function Page() {
   return (
     <>
-      <Canvas>
+      <Canvas shadows>
         <EffectNode projectName={"space-me"}></EffectNode>
+
+        <EffectComposer>
+          <Bloom mipmapBlur></Bloom>
+        </EffectComposer>
+        <OrbitControls
+          target={[0, 0, 29]}
+          rotateSpeed={-1}
+          object-position={[0, 0, 30]}
+          makeDefault
+        ></OrbitControls>
+        <Environment files={[`/hdr/studiolighting.hdr`]}></Environment>
       </Canvas>
+
+      {/*  */}
       <div className=" absolute bottom-0 left-0 px-3 py-3">
         <div>
           <a
