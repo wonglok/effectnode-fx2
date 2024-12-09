7a612fa298b65671385d28f6aa236bcaa4565938
wonglok <yellowhappy831@gmail.com>
2024-12-09 15:05:38 +0800

    save

--- src/effectnode/projects/magic/codes/shader/ball.fs
+++ src/effectnode/projects/magic/codes/shader/ball.fs
@@ -7,14 +7,50 @@ varying vec2 vTUV;
 uniform sampler2D varPosition;
 uniform sampler2D varVelocity;
 
+
+
+mat3 rotation3dX(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    1.0, 0.0, 0.0,
+    0.0, c, s,
+    0.0, -s, c
+  );
+}
+
+
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    c, 0.0, -s,
+    0.0, 1.0, 0.0,
+    s, 0.0, c
+  );
+}
+
+mat3 rotation3dZ(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    c, s, 0.0,
+    -s, c, 0.0,
+    0.0, 0.0, 1.0
+  );
+}
+
+
 void main(void){
     vec4 dataPosition = texture2D(varPosition, vTUV);
     vec4 dataVelocity = texture2D(varVelocity, vTUV);
     
-    
     gl_FragColor = vec4(1.0);
 
-    gl_FragColor.rgb = vec3(brightness) * vec3(normalize(dataVelocity.rgb) * 0.5 + 0.5);
+    gl_FragColor.rgb = vec3(brightness) * vec3(normalize(dataVelocity.rgb) * rotation3dX(dataPosition.x) * rotation3dY(dataPosition.y) * rotation3dZ(dataPosition.z) * 0.5 + 0.5);
 }
 
 // 
--- src/effectnode/projects/magic/codes/shader/ball.vs
+++ src/effectnode/projects/magic/codes/shader/ball.vs
@@ -74,6 +74,45 @@ float cnoise(vec2 P){
   return 2.3 * n_xy;
 }
 
+
+
+mat3 rotation3dX(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    1.0, 0.0, 0.0,
+    0.0, c, s,
+    0.0, -s, c
+  );
+}
+
+
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    c, 0.0, -s,
+    0.0, 1.0, 0.0,
+    s, 0.0, c
+  );
+}
+
+mat3 rotation3dZ(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+
+  return mat3(
+    c, s, 0.0,
+    -s, c, 0.0,
+    0.0, 0.0, 1.0
+  );
+}
+
+
+
+
 void main(void){
     vTUV = tuv;
 
